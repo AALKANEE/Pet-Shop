70f96ae356b77661a180b1e519b6314a86b913ee
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-03-27 13:15:14 +0330

    create functions

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 import json
 import os.path
+
+# inventory class
+class inventory:
+    pets={}
+
+    def __init__(self):
+        pass
+    def add(self):
+        pass
+    def remove(self):
+        pass
+    def display(self):
+        pass
+    def save(self):
+        pass
+    def load(self):
+        pass
