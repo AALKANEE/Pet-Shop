621b21b53818b87db86b64ef07b7d4e15983dd9f
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-03-27 13:30:12 +0330

    adding details to the display and remove def

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ class inventory:
     def __init__(self):
         self.load()
 
-#def add : quantity(qty and q),key,value(v)
+#def: quantity(qty and q),key,value(v)
+        
     def add(self,key,qty):
         q=0
         if key in self.pets:
@@ -18,10 +19,21 @@ class inventory:
             q=qty
         self.pets[key]=q        
         print(f'Added {qty} {key}: total = {self.pets[key]}')
-    def remove(self):
-        pass
+
+    def remove(self,key,qty):
+        q=0
+        if key in self.pets:
+            v=self.pets[key]
+            q=v-qty
+        if q<0:
+            q=0    
+        self.pets[key]=q        
+        print(f'Removed {qty} {key}: total = {self.pets[key]}')
+
     def display(self):
-        pass
+        for key, value in self.pets.items():
+            print(f'{key} = {value}')
+
     def save(self):
         pass
     def load(self):
