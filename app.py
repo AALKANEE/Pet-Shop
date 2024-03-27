df18c29e373a90795d2071a71ce2a50b8432e2c0
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-03-27 13:25:59 +0330

    adding details to the add def

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ class inventory:
     pets={}
 
     def __init__(self):
-        pass
-    def add(self):
-        pass
+        self.load()
+
+#def add : quantity(qty and q),key,value(v)
+    def add(self,key,qty):
+        q=0
+        if key in self.pets:
+            v=self.pets[key]
+            q=v+qty
+        else:
+            q=qty
+        self.pets[key]=q        
+        print(f'Added {qty} {key}: total = {self.pets[key]}')
     def remove(self):
         pass
     def display(self):
